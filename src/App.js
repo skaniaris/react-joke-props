9a27f63f82742b4d086649c6e7c156f0ba436e5f
skaniaris <kaniariss@gmail.com>
2022-09-04 15:04:45 -0400

    adding rating and adding conditional logic to remove punchline for last joke

--- src/App.js
+++ src/App.js
@@ -8,21 +8,26 @@ export default function App() {
       <Joke
       setup="Why does Waldo wear stripes?"
       punchline="Because he doesn’t want to be spotted."
+      rating={2}
       />
       <Joke
       setup="Why did the chicken go to prison?"
       punchline="Crimes."
+      rating={5}
       />
       <Joke
       setup="What do you call a sad cup of coffee?"
       punchline="Depresso."
+      rating={4}
       />
       <Joke
       setup="What did the grape say when the elephant sat on it?"
       punchline="Nothing, but it let out a little wine."
+      rating={3}
       />
       <Joke
       setup="What do you get when you cross a joke with a rhetorical question?"
+      rating={2}
       />
     </main>
   )
--- src/Joke.js
+++ src/Joke.js
@@ -3,8 +3,9 @@ import React from 'react';
 export default function Joke(props) {
     return (
         <div className="joke-box">
-            <h3>{props.setup}</h3>
-            <p>{props.punchline}</p>
+            <h3>Setup: {props.setup}</h3>
+            {props.punchline && <p>Punchline: {props.punchline}</p>}
+            <p>Rating: {props.rating}/5</p>
         </div>
     )
 }
